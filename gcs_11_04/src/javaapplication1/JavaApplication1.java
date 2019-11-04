ad575849953776961f230cb183962554730e93ea
CsabaVK <56678429+CsabaVK@users.noreply.github.com>
2019-11-04 09:36:42 +0100

    Add files via upload

--- gcs_11_04/src/javaapplication1/JavaApplication1.java
+++ gcs_11_04/src/javaapplication1/JavaApplication1.java
@@ -5,6 +5,8 @@
  */
 package javaapplication1;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,6 +16,8 @@ public class JavaApplication1 {
 
     /**
      * @param args the command line arguments
+     * 
+     * sout (tab) = println
      */
     public static void main(String[] args) {
         //Lista létrehoz
@@ -42,11 +46,81 @@ public class JavaApplication1 {
         System.out.println((hanyadik + 1 ) + ". " + nevek.get(hanyadik));
         
         System.out.println(nevek);
+        
+        
+        int elemszám = nevek.size();
+        System.out.println("A lista elemszáma: " + elemszám);
+        //Hány elemet tartalmaz a lista  //(3)
+        for (String elem: nevek) {
+            System.out.println(elem);
+        }
+        
     }
     static void lista_kiir(ArrayList<String> lista)
     {
         int elemszám = lista.size();
-        System.out.println("");
+        System.out.println("A lista elemszáma: " + elemszám);
+        for (String elem: lista) {
+            System.out.println(elem);
+        }
+    }
+    
+    
+    
+    //Részfeladat
+    static void lista_feltolt_0_100(int elemszam, ArrayList<Integer> lista)
+    {
+        //import Random
+        Random r = new Random();
+        for (int i = 0; i <= elemszam; i++) {
+            lista.add(r.nextInt(100));
+        }
+    }
+    //Részfeladat
+    
+    
+    
+    
+    static void lista_kiirINTEGER(ArrayList<Integer> listaINT)
+    {
+        int elemszám = listaINT.size();
+        System.out.println("A lista elemszáma: " + elemszám);
+        for (Integer elem: listaINT) {
+            System.out.println(elem);
+        }
+    }
+    
+    static void masodikfeladat(String[] args)
+    {
+        //Lista létrehoz
+        ArrayList<Integer> szamok = new ArrayList<>();
+        
+        System.out.println("Hány számot szeretne? ");
+        //import scanner
+        Scanner konzol = new Scanner(System.in);
+        int db = konzol.nextInt();
+        
+        //**this madafaka**
+        lista_feltolt_0_100(db,szamok);
+        
+        //??? Lista kiiratása
+        lista_kiirINTEGER(szamok); 
+        
+        //Rendezzük sorba
+        szamok.sort(null);
+        lista_kiirINTEGER(szamok);
+        
+        System.out.println(" számok összege: " + lista_szum(szamok));
+    }
+    
+    //számok összege
+    static int lista_szum(ArrayList<Integer> lista)
+    {
+        int osszeg = 0;
+        for (int x: lista) {
+            osszeg += x;
+        }
+        return osszeg;
     }
     
 }
